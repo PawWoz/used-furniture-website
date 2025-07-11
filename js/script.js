ee3f0699e8e3cf39669c2bca7d63367ca17d9b2f
PawWoz <pawelwozignoj@gmail.com>
2025-07-11 14:05:23 +0200

    Add new files

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let navbar = document.querySelector('.header .navbar');
+let menu = document.querySelector('#menu-btn');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+let cart = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cart.classList.add('active');
+}
+
+document.querySelector('#close-form').onclick = () =>{
+    cart.classList.remove('active');
+}
+
+function openEditDialog(productId) {
+    const dialog = document.getElementById('editDialog');
+    dialog.showModal();
+}
+
+
+
+
